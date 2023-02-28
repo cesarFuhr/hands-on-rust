e448569709190a8069c949e8dffb9dbcae738322
Cesar Cara <cesar.cara@protonmail.com>
2023-02-28 09:42:31 -0300

    hud and a lot of fun stuff

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,22 +8,38 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
+        Health {
+            current: 20,
+            max: 20,
+        },
     ));
 }
 
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
+}
+
 pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    let (hp, name, gliph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
+    };
     ecs.push((
         Enemy,
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
-            glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('o'),
-                2 => to_cp437('O'),
-                _ => to_cp437('g'),
-            },
+            glyph: gliph,
         },
         MovingRandomly {},
+        Health {
+            current: hp,
+            max: hp,
+        },
+        Name(name),
     ));
 }
